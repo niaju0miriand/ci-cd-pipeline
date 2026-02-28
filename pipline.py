72a6c50e7cfd638d042156ab81b448636a7b3ddf
niaju0miriand <niaju0miriand@myswissmail.ch>
2026-02-28 15:13:04 +0700

    Create pipline.py

---
+++ pipline.py
@@ -0,0 +1,286 @@
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+# Not exactly sure what goes in this file.
+
+# In any case the code below was loaded and tested on my
+# NodeMCU as main.py which is also in the NodeMCU directory.
+
+
+
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
